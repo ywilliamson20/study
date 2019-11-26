3c73d92fa86953f39703c346e689ed755c0ebac9
Gerard Avecilla <gerardavecilla@ufl.edu>
2019-11-25 22:05:09 -0500

    added default message

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -4,31 +4,36 @@ import { View } from 'react-native';
 
 export default class Chat extends React.Component {
   state = {
-    messages: []
+    messages: [],
   };
   componentDidMount() {
     this.setState({
       messages: [
         {
-          _id: 1,
-          text: "Testtesttest",
+          _id: 1, 
+          text: "Hey, I'm sitting in the back corner of the library! Feel free to stop by and study with me!",
           createdAt: new Date(),
           user: {
-            _id: 1,
+            _id: 2,
             name: "Steve",
-            avatar: "http://placeimg.com/140/140/people"
+            avatar: "http://placeimg.com/140/140/people",
           }
         }
       ]
     });
-    
   }
+  onSend = (messages = []) => {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }))
+  };
+
   render() {
     return (
       <View style={{width: 300, height: 500}}>
         <GiftedChat 
           messages={this.state.messages}
-          //onSend = {}
+          onSend = {messages => this.onSend(messages)}
         />
       </View>
     );
